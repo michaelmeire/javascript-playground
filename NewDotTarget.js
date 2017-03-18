68e6e06abce8e978d33a3328c5bfdc7173920bbe
bkmic <Hiphiprahoe0305>
2017-03-18 09:56:14 +0100

    [michael] small changes

--- NewDotTarget.js
+++ NewDotTarget.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function Person(name) {
-    console.log(`${name} --  "typeof new.target" --> ${(typeof new.target)}`);
+    console.log(`${name} --  "typeof new.target" --> ${typeof new.target}`);
     console.log(`${name} --  "typeof new.target !== 'undefined'" --> ${typeof new.target !== 'undefined'}`);
     console.log(`${name} --  "typeof new.target === Person" --> ${typeof new.target === Person}`);
     console.log(`${name} --  "new.target === Person" --> ${new.target === Person}`);
@@ -15,9 +15,13 @@ var person = new Person("Person");
 console.log(`"typeof Person" --> ${typeof Person}`);
 console.log(`"typeof Person.constructor" --> ${typeof Person.constructor}`);
 console.log(`"typeof person.constructor" --> ${typeof person.constructor}`);
+console.log(`"typeof Function.constructor" --> ${typeof Function.constructor}`);
 console.log(`"person.constructor === Person" --> ${person.constructor === Person}`);
 console.log(`"person.constructor === Person.constructor" --> ${person.constructor === Person.constructor}`);
 console.log(`"Person.constructor === Person" --> ${Person.constructor === Person}`);
 console.log(`"Person.constructor === Function" --> ${Person.constructor === Function}`);
 
 var anotherPerson = new AnotherPerson("AnotherPerson");
+console.log(`"typeof anotherPerson.constructor" --> ${typeof anotherPerson.constructor}`);
+console.log(`"anotherPerson.constructor === Person" --> ${anotherPerson.constructor === Person}`);
+console.log(`"anotherPerson.constructor === AnotherPerson" --> ${anotherPerson.constructor === AnotherPerson}`);
