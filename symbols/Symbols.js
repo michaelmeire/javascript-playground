6829480ec2a750cb8c5e230ee14c9bb07ab6f29f
bkmic <michael.meire@cegeka.com>
2017-04-03 17:09:48 +0200

    [michael] Symbols

--- symbols/Symbols.js
+++ symbols/Symbols.js
@@ -6,3 +6,24 @@ person[firstName] = "Nicholas";
 log(() => "first name" in person);
 log(() => firstName in person);
 log(() => person[firstName]);
+
+let lastName = Symbol("last name");
+person[lastName] = "Ambrosi";
+log(() => person[lastName]);
+
+let person2 = {
+    "key": "value",
+    [firstName]: "some",
+    [lastName]: "guy"
+}
+log(() => person2[firstName]);
+log(() => person2[lastName]);
+log(() => person2);
+log(() => Object.keys(person2));
+log(() => Object.getOwnPropertyNames(person2));
+const symbols = Object.getOwnPropertySymbols(person2);
+console.log(symbols);
+for (symbol in symbols) {
+    console.log(symbol.toString());
+}
+
