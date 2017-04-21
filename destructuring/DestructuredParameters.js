4f154d7ba7dd9823ea576f478095509b27944709
bkmic <michael.meire@cegeka.com>
2017-04-21 13:06:40 +0200

    [michael] destructured parameters

--- destructuring/DestructuredParameters.js
+++ destructuring/DestructuredParameters.js
@@ -12,3 +12,15 @@ setCookie("type", "js", {
     secure: true,
     expires: 60000
 });
+
+function setCookieWithDefaultValues(name, value, {secure = true, path = "dummy", domain = "dummydomain", expires = 60000} = {}) {
+    console.log(name);
+    console.log(value);
+    console.log(secure);
+    console.log(path);
+    console.log(domain);
+    console.log(expires);
+}
+setCookieWithDefaultValues("type", "js");
+setCookieWithDefaultValues("type", "js", {expires: 75000});
+
